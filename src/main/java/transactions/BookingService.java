84f98f0f7f923f4d9595390342ffd6a2ca25584d
WDB40 <wbrown1640@gmail.com>
2018-09-04 21:21:34 -0500

    Removing the DB connection and adding the @Transactional

--- src/main/java/transactions/BookingService.java
+++ src/main/java/transactions/BookingService.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Component
@@ -17,6 +18,7 @@ public class BookingService {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    @Transactional
     public void book(String... persons){
         for(String person : persons) {
             logger.info("Booking " + person + " in a seat...");
